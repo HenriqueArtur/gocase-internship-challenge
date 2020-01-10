91c33202757bcce6f58fe465771e732a93c11ff8
Artur <henriqueacg@gmail.com>
2020-01-10 17:32:00 -0300

    create order finish

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -6,10 +6,22 @@ class Api::V1::OrdersController < Api::V1::ApiController
   end
 
   def create
-    if check_delivery_service && check_reference && check_purchase_channel
-      render json: {resultado: 'deu certo!'}
+    if check_delivery_service && check_reference_lenght && check_purchase_channel
+      if exit? == false
+        order = Order.new(reference: params[:reference], purchase_channel: params[:purchase_channel], client_name: params[:client_name],
+                          address: params[:address], delivery_service: params[:delivery_service], total_value: params[:total_value],
+                          line_items: params[:line_items], status: 'ready')
+        
+        if order.save
+          render json: {status: 'SUCCESS', menssage:'Loaded Order', data:order}, status: :ok
+        else
+          render json: {status: 'ERROR', menssage:'Order not saved'}, status: :unprocessable_entity
+        end
+      else
+        render json: {status: 'ERROR', menssage:'Reference already exists'}, status: :unprocessable_entity
+      end
     else
-      render json: {resultado: 'deu errado!'}
+      render json: {status: 'ERROR', menssage:'Invalid params'}, status: :unprocessable_entity
     end
   end
 
@@ -24,7 +36,7 @@ class Api::V1::OrdersController < Api::V1::ApiController
   end
 
   private
-    def check_reference
+    def check_reference_lenght
       params[:reference].to_s.length === 8 ? true : false
     end
 
@@ -44,6 +56,17 @@ class Api::V1::OrdersController < Api::V1::ApiController
 
       dsArray.each do |ds|
         if ds == params[:delivery_service]
+          return true
+        end
+      end
+      return false
+    end
+
+    def exit?
+      orders = Order.all
+
+      orders.each do |order|
+        if order.reference == params[:reference]
           return true
         end
       end
