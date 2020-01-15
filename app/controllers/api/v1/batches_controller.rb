7bf60abb500cd315ef535591a7fe6f2559403b1b
Artur <henriqueacg@gmail.com>
2020-01-15 00:20:10 -0300

    create +/-

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -14,42 +14,40 @@ class Api::V1::BatchesController < Api::V1::ApiController
         order.batch = batch
         order.update_attribute(:status, 'production')
       end
+
       if batch.save
-        render json: {status: 'SUCCESS', menssage:'Loaded Order', batch:batch, group_of_orders: batch.orders}, status: :ok
+        render json: {status: 'SUCCESS', menssage:'Loaded Order', data:batch, orders_data: orders}, status: :created
       else
-        render json: {status: 'ERROR', menssage:'Batch not saved'}, status: :unprocessable_entity
+        renderJSON('ERROR', 'Batch not saved', :unprocessable_entity)
       end
     else
-      render json: {status: 'ERROR', menssage:'Invalid Puchase Channel'}, status: :unprocessable_entity
+      !check_purchase_channel ? renderJSON('ERROR', 'Invalid Puchase Channel', :unprocessable_entity) : renderJSON('ERROR', 'No Orders to create a Batch', :unprocessable_entity)
     end
   end
 
   def mark_as_closing
-    if Batch.exists?(['reference LIKE ?', params[:reference]])
-      orders = Batch.where(reference: params[:reference]).take.orders
-      orders.each do |order|
-        order.update_attribute(:status, 'closing')
-      end
-      render json: {status: 'SUCCESS', menssage:'Orders Updated'}, status: :ok
-    else
-      render json: {status: 'ERROR', menssage:'Batch dont exists'}, status: :unprocessable_entity
-    end
+    Batch.exists?(['reference LIKE ?', params[:reference]]) ? order_status_to_closing : renderJSON('ERROR', 'batch dont finded', :ok)
   end
 
   def mark_as_sent
-    if Batch.exists?(['reference = ? AND purchase_channel = ?', params[:reference], params[:purchase_channel]])
-      if order_is_closing?
-        orders = Batch.where(reference: params[:reference]).take.orders
-        orders.each do |order|
-          order.update_attribute(:status, 'sent')
-        end
-        render json: {status: 'SUCCESS', menssage:'Orders Updated', data: orders}, status: :ok
-      else
-        render json: {status: 'ERROR', menssage:'Orders Batch dont closing'}, status: :unprocessable_entity
-      end
-    else
-      render json: {status: 'ERROR', menssage:'Batch dont exists'}, status: :unprocessable_entity
-    end
+    # if Batch.exists?(['reference = ? AND delivery_service = ?', params[:reference], params[:delivery_service]])
+    #   order_is_closing? ? order_status_to_sent : renderJSON('ERROR', 'Orders in Batch dont closing', :ok)
+    #   # if order_is_closing?
+    #   #   orders = Batch.where(reference: params[:reference]).take.orders
+    #   #   orders.each do |order|
+    #   #     order.update_attribute(:status, 'sent')
+    #   #   end
+    #   #   render json: {status: 'SUCCESS', menssage:'Orders Updated', data: orders}, status: :ok
+    #   # else
+    #   #   render json: {status: 'ERROR', menssage:'Orders in Batch dont closing'}, status: :unprocessable_entity
+    #   # end
+    # else
+    #   renderJSON('ERROR', 'Batch dont finded', :unprocessable_entity) 
+    # end
+    #batch = Batch.includes(:orders)
+    #orders = Order.includes(:batch).where('batch.reference = ?', params[:reference]).references(:batch)
+    orders = Batch.where(reference: params[:reference]).take.orders.where('delivery_service = ?', params[:delivery_service])
+    renderJSON(' ', ' ', :ok, orders)
   end
 
   private
@@ -63,4 +61,20 @@ class Api::V1::BatchesController < Api::V1::ApiController
       end
     end
 
+    def order_status_to_closing
+      orders = Batch.where(reference: params[:reference]).take.orders
+      orders.each do |order|
+        order.update_attribute(:status, 'closing')
+      end
+      renderJSON('SUCCESS', "#{orders.length} orders updated to 'closing'", :ok, orders)
+    end
+
+    def order_status_to_sent
+      orders = Batch.where([reference: params[:reference]]).take.orders.where([delivery_service: params[:delivery_service]]).take
+      #orders = Batch.where(['reference = ? AND delivery_service = ?', params[:reference], params[:delivery_service]]).take.orders
+      orders.each do |order|
+        order.update_attribute(:status, 'closing')
+      end
+      renderJSON('SUCCESS', "#{orders.length} orders updated to 'sent'", :ok, orders)
+    end
 end
